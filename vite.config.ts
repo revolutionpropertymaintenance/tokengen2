e38d0253e99e20c75584767f00dc49e2307e3a38
cryptosacloud <cryptos.acloud.host@gmail.com>
2025-07-13 21:08:42 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { Buffer } from 'buffer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     global: 'globalThis',
-    Buffer: 'globalThis.Buffer',
+    Buffer: Buffer,
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
